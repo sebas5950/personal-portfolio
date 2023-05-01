e412d4a51b17b044272a6ce0ae524a4cf2a11491
Alex Guanipatin <sebas5950@gmail.com>
2023-05-01 18:25:36 -0400

    done

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main className="">
-      <p>what do i write here i wonder</p>
+     
       <section className='bg-blue-700 h-screen min-h-screen'>hello</section>
     </main>
   )
