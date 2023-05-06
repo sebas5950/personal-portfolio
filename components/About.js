3f228cfdcaab519b891e17f7174caba2dff3911a
Alex Guanipatin <sebas5950@gmail.com>
2023-05-06 13:04:51 -0400

    working on about text

--- components/About.js
+++ components/About.js
@@ -20,6 +20,9 @@ export default function About() {
         src='https://scontent-lga3-2.xx.fbcdn.net/v/t1.6435-9/180978949_314228950059549_1005358403722529104_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=fDrxhzz6GZ0AX9lrTen&_nc_oc=AQkpr1Tk0enNJmIbpBYH54d9HP6vHFQRG0xVZahjAWctKr0WLMgacp_rBCLRpCd25so&_nc_ht=scontent-lga3-2.xx&oh=00_AfCBzGeMiO3TZ5M6KtTHL802rc9r3hWIbYQBg210jguSow&oe=647BE48E'
         className='-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px]'
         />
+        <div className='space-y-10 px-10 md:px-10'>
+            <h4 className='text-4xl font-semibold '>Here is a <span className=''>little</span> little background</h4>
+        </div>
     </div>
   )
 }
