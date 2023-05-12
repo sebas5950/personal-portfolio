7e68a57d1b103212314002ba7c8147abc2ac3658
Alex Guanipatin <sebas5950@gmail.com>
2023-05-11 21:02:19 -0400

    done

--- components/Projects.js
+++ components/Projects.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Projects() {
   const projects = [1, 2, 3, 4, 5];
   return (
-    <div className="h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0">
+    <div className="h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0 ">
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
         Projects
       </h3>
