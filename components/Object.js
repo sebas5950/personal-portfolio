b6a1c97e4968a320d6e3a3fb90c9cf62b2bbc974
Alex Guanipatin <sebas5950@gmail.com>
2023-05-17 22:53:36 -0400

    working on projects

--- components/Object.js
+++ components/Object.js
@@ -1,6 +1,40 @@
-const projects = [ {
-    id: 1,
-    title: "",
-    languages: "",
-    imgage: "",
-}]
+const Object = [
+    {
+        id: 1,
+        title: "Nerdnook",
+        description: "",
+        technologies: "",
+        img: "",
+        giturl: "",
+        url: "",
+    },
+    {
+        id: 2,
+        title: "DnD Monster Finder",
+        description: "",
+        technologies: "",
+        img: "",
+        giturl: "",
+        url: "",
+    },
+    {
+        id: 3,
+        title: "Gunters",
+        description: "",
+        technologies: "",
+        img: "",
+        giturl: "",
+        url: "",
+    },
+    {
+        id: 4,
+        title: "League of Legends item builder",
+        description: "",
+        technologies: "",
+        img: "",
+        giturl: "",
+        url: "",
+    },
+]
+
+export default Object
