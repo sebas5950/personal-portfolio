668a02feb7cdef6a3d1ef69f8d17dfb5f471c0ba
Alex Guanipatin <sebas5950@gmail.com>
2023-05-24 22:03:14 -0400

    working on projects then done

--- components/Projects.js
+++ components/Projects.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 import Object from "./Object";
 import Image from "next/image";
 
+
 export default function Projects() {
-  console.log(Object);
+
+  const [current, setCurrent] = useState(0)
+  const length = Object.length
+  
+  function nextSlide(){
+    setCurrent(current === length - 1 ? 0 : current + 1)
+  }
+
+  function prevSlide(){
+    setCurrent(current === 0 ? length - 1 : current - 1)
+  }
+
+  if(!Array.isArray(Object) ||  Object.length <= 0) {
+    return null
+  }
 
   return (
     <div className="h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0 ">
@@ -11,9 +26,9 @@ export default function Projects() {
         Projects I am back at it no giving up
       </h3>
       <div className="relative flex w-full overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20">
-        {Object.map((project) => (
+        {Object.map((project, index) => (
           <div
-            key={project.id}
+            key={index}
             className="w-screen flex flex-shrink-0 snap-center flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen"
           >
             <img src={project.img} alt="Project Image" />
