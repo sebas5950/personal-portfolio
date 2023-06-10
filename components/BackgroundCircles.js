0d5e95d4485e0dd350ef8a7a24b32c765b503650
Alex Guanipatin <sebas5950@gmail.com>
2023-06-09 23:55:18 -0400

    done

--- components/BackgroundCircles.js
+++ components/BackgroundCircles.js
@@ -16,10 +16,10 @@ export default function BackgroundCircles() {
     }}
     className='relative flex justify-center items-center'>
         <div className='absolute border border-[#9b0fc2] rounded-full h-[250px] w-[250px] mt-52 animate-ping'/>
-        <div className='absolute border border-[#6c4e75] rounded-full h-[300px] w-[300px] mt-52'/>
-        <div className='absolute border border-[#6c4e75] rounded-full h-[500px] w-[500px] mt-52'/>
+        <div className='absolute border border-[#6b4d74] rounded-full h-[300px] w-[300px] mt-52'/>
+        <div className='absolute border border-[#6b4d74] rounded-full h-[500px] w-[500px] mt-52'/>
         <div className='rounded-full border border-[#9b0fc2] opacity-20 h-[650px] w-[650px] absolute mt-52 animate-pulse'/>
-        <div className='absolute border border-[#6c4e75] rounded-full h-[800px] w-[800px] mt-52'/>
+        <div className='absolute border border-[#6b4d74] rounded-full h-[800px] w-[800px] mt-52'/>
     </motion.div>
   )
 }
